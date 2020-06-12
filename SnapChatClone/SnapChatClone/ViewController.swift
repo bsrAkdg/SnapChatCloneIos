de0ae76b39684f595d1f6a9b3a12fd644d959245
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-12 16:05:16 +0300

    Project create

---
+++ SnapChatClone/SnapChatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChatClone
+//
+//  Created by Büşra Serdaroğlu on 12.06.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
