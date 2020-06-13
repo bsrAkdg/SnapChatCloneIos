116a7b2cafe5bd0eb02c56ed57ec6b9443c78a0c
bsrAkdg <busraaakdag@gmail.com>
2020-06-13 15:34:20 +0300

    Select image picker

--- SnapChatClone/SnapChatClone/viewControllers/UploadVC.swift
+++ SnapChatClone/SnapChatClone/viewControllers/UploadVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 @IBOutlet weak var image: UIImageView!
     
@@ -16,6 +16,24 @@ override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
+    
+    image.isUserInteractionEnabled = true
+    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePicture))
+    image.addGestureRecognizer(gestureRecognizer)
+}
+    
+@objc func choosePicture() {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.sourceType = .photoLibrary
+    self.present(picker, animated: true, completion: nil)
+    
+}
+    
+func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    
+    image.image = info[.originalImage] as? UIImage
+    self.dismiss(animated: true, completion: nil)
 }
     
 @IBAction func button_upload(_ sender: Any) {
