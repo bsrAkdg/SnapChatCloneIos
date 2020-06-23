ea88aa5499fc34f2a84923edce92394e126da627
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-23 22:55:31 +0300

    Get data from firestore

---
+++ SnapChatClone/SnapChatClone/Model/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  SnapChatClone
+//
+//  Created by Büşra Serdaroğlu on 23.06.2020.
+//  Copyright © 2020 Busra Serdaroglu. All rights reserved.
+//
+
+import Foundation
+
+struct Snap {
+    var username: String
+    var imageUrlArray : [String]
+    var date: Date
+}
--- SnapChatClone/SnapChatClone/viewControllers/FeedVC.swift
+++ SnapChatClone/SnapChatClone/viewControllers/FeedVC.swift
@@ -13,10 +13,11 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var snapTableView: UITableView!
     
     let fireStoreDatabase = Firestore.firestore()
+    var snapArray = [Snap]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         snapTableView.delegate = self
         snapTableView.dataSource = self
@@ -29,8 +30,31 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
             } else {
                 if snapShot != nil && snapShot?.isEmpty == false {
+                    self.snapArray.removeAll() // clear array
                     for document in snapShot!.documents {
+                        
+                        let documentId = document.documentID
+                        
                         // create custom list
+                        if let username = document.get("snapOwner") as? String {
+                            if let imageUrlArray = document.get("imageUrlArray") as? [String] {
+                                if let date = document.get("date") as? Timestamp {
+                                    
+                                    if let difference = Calendar.current.dateComponents([.hour], from: date.dateValue(), to: Date()).hour {
+                                        if difference >= 24 {
+                                            // delete
+                                            self.fireStoreDatabase.collection("Snaps").document(documentId).delete { (error) in
+                                                
+                                            }
+                                        }
+                                        // TIMELEFT -> SNAPVC
+                                    }
+                                    let snap = Snap(username: username, imageUrlArray: imageUrlArray, date: date.dateValue())
+                                    self.snapArray.append(snap)
+                                    
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -49,14 +73,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                             if let username = document.get("username") as? String {
                                 UserSingleton.sharedUserInfo.email = Auth.auth().currentUser!.email!
                                 UserSingleton.sharedUserInfo.username = username
-
+                                
                             }
                         }
                     }
                 }
         }
     }
-
+    
     func makeAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
@@ -65,14 +89,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return snapArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        
-        cell.username.text = "text"
-        
+        let snap = snapArray[indexPath.row]
+        cell.username.text = snap.username        
         return cell
     }
 }
