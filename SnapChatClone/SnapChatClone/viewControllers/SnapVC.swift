95ab17c6bf395da3ed24bf288e3858ccd4291ec6
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-23 23:48:12 +0300

    ImageSlideshow

--- SnapChatClone/SnapChatClone/viewControllers/SnapVC.swift
+++ SnapChatClone/SnapChatClone/viewControllers/SnapVC.swift
@@ -7,32 +7,57 @@
 //
 
 import UIKit
+import ImageSlideshow
 
 class SnapVC: UIViewController {
-
+    
     @IBOutlet weak var timeLabel: UILabel!
     
     var selectedSnap : Snap?
     var selectedTime : Int?
+    var inputArray = [KingfisherSource]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let time = selectedTime {
             timeLabel.text = "Time left : \(time)"
         }
+        
+        if let snap = selectedSnap {
+            for imageUrl in snap.imageUrlArray {
+                inputArray.append(KingfisherSource(urlString: imageUrl)!)
+            }
+            
+            let imageSlideShow = ImageSlideshow(frame: CGRect(x: 10, y: 10, width: self.view.frame.width * 0.95, height: self.view.frame.height * 0.9))
+            
+            // indicator
+            let pageIndicator = UIPageControl()
+            pageIndicator.currentPageIndicatorTintColor = UIColor.lightGray
+            pageIndicator.pageIndicatorTintColor = UIColor.black
+            imageSlideShow.pageIndicator = pageIndicator
+            
+            imageSlideShow.backgroundColor = UIColor.white
+            imageSlideShow.contentScaleMode = UIViewContentMode.scaleAspectFit
+            imageSlideShow.setImageInputs(inputArray)
+            
+            self.view.addSubview(imageSlideShow)
+            self.view.bringSubviewToFront(timeLabel)
+            
+        }
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
